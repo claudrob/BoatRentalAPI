a5cd7e60b3e9b8befe112ebeb33b4d2f8be1480a
claud <crobinson@tec.mx>
2024-06-25 22:57:47 -0600

    Adding the readme.md file.

---
+++ src/main/java/com/petscreening/BoatRentalAPI/config/ValidatorConfig.java
@@ -0,0 +1,17 @@
+package com.petscreening.BoatRentalAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+@Configuration
+public class ValidatorConfig {
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        return validatorFactory.getValidator();
+    }
+}
